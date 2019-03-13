0b4dd5dfac0c5a18b70176b4b868ff67cb7204b8
Caspidev <pmcaspi@gmail.com>
2019-03-13 12:03:07 +0200

    push parcel again

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,2 +1,2 @@
-export { default as SMSSender } from './SMSSender'
-export { default as CameraApp } from './Camera'
+//export { default as SMSSender } from './SMSSender'
+//export { default as CameraApp } from './Camera'
